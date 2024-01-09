07a42780434574e7a9c0a23fce65597d2ca73b4c
Siyarbekir47 <146875165+Siyarbekir47@users.noreply.github.com>
2024-01-10 00:06:42 +0100

    overall changes

--- myPatcher/main.cpp
+++ myPatcher/main.cpp
@@ -3,94 +3,141 @@
 #include <string>
 #include "nlohmann/json.hpp"
 #include <fstream>
+#include <sstream>
 
+#include <filesystem>
+
+
+
+namespace fs = std::filesystem;
 
 const char* red = "\033[31m";
 const char* reset = "\033[0m";
 const char* green = "\033[92m";
 
 // Callback function for writing data received by libcurl
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* data) {
-
-    data->append(static_cast<char*>(contents), size * nmemb);
+size_t WriteData(void* ptr, size_t size, size_t nmemb, std::ofstream* outFile) {
+    outFile->write((char*)ptr, size * nmemb);
     return size * nmemb;
+}
+
+
+
+
+bool DownloadFile(const std::string& url, const std::string& localPath) {
+    CURL* curl;
+    std::ofstream file;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl) {
+        file.open(localPath, std::ios::binary);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file: " << localPath << std::endl;
+            curl_easy_cleanup(curl);
+            return false;
+        }
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteData);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+            file.close();
+            curl_easy_cleanup(curl);
+            return false;
+        }
+
+        file.close();
+        curl_easy_cleanup(curl);
+    }
+    else {
+        return false;
+    }
+
+    return true;
+}
+
+bool UpdateFile(const std::string& oldFilePath, const std::string& newFilePath)
+{
+    //Check if the new file exists
+    std::ifstream newFile(newFilePath);
+    if (!newFile.good())
+    {
+		std::cerr << red << "New file not found" << reset << std::endl;
+		return false;
+    }
+    //delete the old file
+    if (std::remove(oldFilePath.c_str()) != 0)
+    {
+        std::cerr << red << "Error deleting file" << oldFilePath << reset << std::endl;
+        return false;
+    }
+    if (std::rename(newFilePath.c_str(), oldFilePath.c_str()) != 0)
+    {
+		std::cerr << red << "Error renaming file" << reset << std::endl;
+		return false;
+	}   
+	return true;
 
 }
 
 
 
-// Function to fetch the manifest file
-std::string FetchManifest(const std::string& url) {
-    std::string response_data;
-    CURLcode result = curl_global_init(CURL_GLOBAL_DEFAULT);
-    if (result != CURLE_OK) {
-        std::cerr << red << "curl_global_init() failed: " << curl_easy_strerror(result) << reset <<std::endl;
-    }
-    else
+bool createFolderIfNotExist(const std::string& folderPath)
+{
+    try
     {
-        std::cout << green << "curl_global_init() succeeded with status: " << curl_easy_strerror(result) << reset << std::endl;
-    }
-   
-    CURL* curl = curl_easy_init();
-    if (curl == NULL) {
-        std::cerr << red << "curl = curl_easy_init() failed" << reset << std::endl;
-    }
-    else
-    {
-		std::cout << green <<"curl = curl_easy_init() succeeded" << reset <<std::endl;
-	}
-
-
-    if (curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
-
-		result = curl_easy_perform(curl);
-        if (result != CURLE_OK) {
-   			std::cerr << red << "curl_easy_perform() failed: " << curl_easy_strerror(result) << reset << std::endl;
-		}
+        
+        if (fs::exists(folderPath))
+        {
+            std::cout << green << "Folder already exists." << reset << std::endl;
+            return true;
+        }
+        
         else
         {
-			std::cout << green << "curl_easy_perform() succeeded with status: " << curl_easy_strerror(result) << reset << std::endl;
+			std::cerr << red << "Folder dont exists" << reset << std::endl;
+            fs::create_directories(folderPath);
+            if (fs::exists(folderPath))
+            {
+				std::cout << green << "Folder created successfully" << reset << std::endl;
+				return true;
+			}
+            else
+            {
+				std::cerr << red << "Error creating folder" << reset << std::endl;
+				return false;
+			}
 		}
-	}
+    }
+    catch (const fs::filesystem_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
-
-    return response_data;
-}   
-
-
-void saveFile(std::string data, std::string filename)
-{
-
-    std::ofstream outFile("manifest.json");
-    if (outFile)
-    {
-		outFile << data;
-		outFile.close();
-        std::cout << green << "\nManifest.json saved successfully" << reset << std::endl;
-	}
-    else
-    {
-		std::cout << red << "Error opening file" << reset << std::endl;
-	}
-
+    return false;
 }
-
-
+ 
 
 int main()
 {
 
     std::string manifestUrl = "https://siyargame.de/manifest/manifest.json"; // url to check for updates
-    std::string manifest = FetchManifest(manifestUrl);
 
-    std::cout << "Manifest data: " << manifest << std::endl;
+    std::string folderPath = "update/";
 
-    saveFile(manifest, "manifest.json");
+    createFolderIfNotExist(folderPath);
+    DownloadFile(manifestUrl, "update/manifest.json");
+
+
+
+
+    std::ifstream file("manifest.json");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string manifestContent = buffer.str();
 
     return 0;
 }
@@ -100,111 +147,3 @@ int main()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-// Callback function for writing data received by libcurl
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp) {
-    userp->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
-
-// Function to fetch the manifest file
-std::string FetchManifest(const std::string& url) {
-    CURL* curl;
-    CURLcode res;
-    std::string response_data;
-
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
-
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-        }
-
-        curl_easy_cleanup(curl);
-    }
-
-    curl_global_cleanup();
-    return response_data;
-}
-
-int main() {
-    std::string manifestUrl = "https://siyargame.de/manifest/manifest.json"; // url to check for updates
-    std::string manifest = FetchManifest(manifestUrl);
-
-    std::cout << "Manifest data:" << std::endl;
-    std::cout << manifest << std::endl;
-
-    return 0;
-}
-
-
-
-
-*/
